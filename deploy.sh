1dc1ee85cbead7b88426d35bb559df9de1613c54
Juan Carcamo <carcamobest1@gmail.com>
2020-05-26 15:02:08 -0500

    pr8

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@
 php artisan cache:clear 
 php artisan config:clear
 php artisan config:cache
-php artisan migrate:refresh --seed --force
+# php artisan migrate:refresh --seed --force
+php artisan migrate:fresh --seed 
 
 #Run Seeds
 
