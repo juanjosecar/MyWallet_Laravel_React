6c93e539205caf6d998b31e7c6c7533ef80bfc0e
Juan Carcamo <carcamobest1@gmail.com>
2020-05-26 14:13:42 -0500

    or2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #Run Database Migrations
 
-php artisan migrate:refresh
+# php artisan migrate:fresh
 # php artisan config:cache
-# php artisan migrate --force
+php artisan migrate --force
+
 #Run Seeds
 
 php artisan db:seed 
